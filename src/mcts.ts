430b6759c93b598386f963fac5e139805c5fd953
Pedro Sousa <pedronsousa@gmail.com>
2020-01-21 18:26:36 +0000

    Fix expansion and rollout

--- src/mcts.ts
+++ src/mcts.ts
@@ -2,20 +2,23 @@ import * as R from "ramda"
 
 export const NO_PARENT = -1
 
-type State = {}
-type Move = {}
+type State = {
+  id: string
+}
+type Move = {
+  id: string
+}
 
 export interface Tree {
-  root: Node
   nodes: ReadonlyArray<Node>
 }
 
 interface Node {
-  state: State
-  parentIndex: number
-  children: ReadonlyArray<Node>
+  children: ReadonlyArray<number>
   index: number
   move?: Move
+  parentIndex: number
+  state: State
   value: number
   visits: number
 }
@@ -24,7 +27,7 @@ export interface Strategy {
   availableMoves: (state: State) => Move[]
   calcUcb: (node: Node) => number
   calcValue: (state: State) => number
-  getNextMove: (state: State) => Move
+  nextMove?: (state: State) => Move | undefined
   isFinal: (state: State) => boolean
   nextState: (state: State, move: Move) => State
 }
@@ -44,33 +47,113 @@ const createNode = (
   visits: 0,
 })
 
-export const createTree = (strategy: Strategy) => (initialState: State) => {
-  const node = createNode(initialState, 0)
-  const childrenMoves = strategy.availableMoves(initialState)
-  const children = childrenMoves.map((move, i) => createNode(strategy.nextState(initialState, move), i + 1, 0, move))
-  const root = {
+const addChildNodes = (strategy: Strategy) => (tree: Tree, node: Node) => {
+  const nodeIndex = tree.nodes.length
+  const childrenMoves = strategy.availableMoves(node.state)
+  const children = childrenMoves.map((move, i) =>
+    createNode(strategy.nextState(node.state, move), nodeIndex + i, node.index, move),
+  )
+
+  const nodeWithChildren = {
     ...node,
-    children,
+    children: children.map(c => c.index),
   }
-  return { nodes: [root] }
+  return {
+    node: nodeWithChildren,
+    tree: {
+      ...tree,
+      nodes: [...tree.nodes.map(n => (n.index === node.index ? nodeWithChildren : n)), ...children],
+    },
+  }
 }
 
-const rollOut = (strategy: Strategy) => (state: State): number => {
-  return strategy.isFinal(state)
-    ? strategy.calcValue(state)
-    : rollOut(strategy)(strategy.nextState(state, strategy.getNextMove(state)))
+const getNode = (tree: Tree) => (index: number) => tree.nodes[index]
+
+const getParent = (tree: Tree) => (node: Node) =>
+  node.parentIndex === NO_PARENT ? undefined : getNode(tree)(node.parentIndex)
+
+export const getRoot = (tree: Tree) => getNode(tree)(0)
+
+const getChildren = (tree: Tree) => (node: Node) => node.children.map(getNode(tree))
+
+const replaceNode = (tree: Tree) => (node: Node) => ({
+  ...tree,
+  nodes: tree.nodes.map(n => (n.index === node.index ? node : n)),
+})
+
+export const createTree = (strategy: Strategy) => (initialState: State) => {
+  const root = createNode(initialState, 0)
+  const initialTree = {
+    nodes: [root],
+  }
+  const { tree } = addChildNodes(strategy)(initialTree, root)
+
+  return tree
 }
 
-export const findBestMove = (strategy: Strategy) => (node: Node) => {
-  const ucbs = node.children.map(c => strategy.calcUcb(c))
+const nextRandomMove = (strategy: Strategy) => (state: State) => {
+  const moves = strategy.availableMoves(state)
+  return moves[Math.floor(Math.random() * moves.length)]
+}
+
+const selectBestUcbNode = (strategy: Strategy) => (tree: Tree, node: Node): Node => {
+  const ucbs = node.children.map(index => strategy.calcUcb(getNode(tree)(index)))
+  console.log("UCBS=====>\n", ucbs)
   const bestUcb = R.reduce(R.max, -Infinity, ucbs)
   const bestNodeIndex = ucbs.findIndex(v => v === bestUcb)
-  const bestNode = node.children[bestNodeIndex]
+  const bestNode = getNode(tree)(node.children[bestNodeIndex])
+  return bestNode.children.length === 0 ? bestNode : selectBestUcbNode(strategy)(tree, bestNode)
+}
 
-  if (strategy.isFinal(bestNode.state)) {
-    return bestNode.move
-  }
+const expand = (strategy: Strategy) => (tree2: Tree, node2: Node) => {
+  const { tree, node } = addChildNodes(strategy)(tree2, node2)
 
-  rollOut(strategy)(bestNode.state)
-  return bestNode.move
+  return visit(strategy)(tree, node)
+}
+
+const rollout = (strategy: Strategy) => (state: State): number => {
+  const nextMove = strategy.nextMove ? strategy.nextMove(state) : nextRandomMove(strategy)(state)
+  return nextMove ? rollout(strategy)(strategy.nextState(state, nextMove)) : strategy.calcValue(state)
+}
+
+const backPropagate = (tree: Tree) => (value: number, node?: Node): Tree =>
+  node
+    ? backPropagate(replaceNode(tree)({ ...node, value: node.value + value, visits: node.visits + 1 }))(
+        value,
+        getParent(tree)(node),
+      )
+    : tree
+
+const visit = (strategy: Strategy) => (tree: Tree, node: Node): Tree => {
+  const bestUcbNode = selectBestUcbNode(strategy)(tree, node)
+
+  console.log("NODE=====>\n", node)
+  console.log("BEST=====>\n", bestUcbNode, strategy.calcValue(bestUcbNode.state), strategy.isFinal(bestUcbNode.state))
+
+  const newTree = strategy.isFinal(bestUcbNode.state)
+    ? bestUcbNode.visits === 0
+      ? backPropagate(tree)(strategy.calcValue(bestUcbNode.state), bestUcbNode)
+      : tree
+    : bestUcbNode.visits === 0
+    ? expand(strategy)(tree, bestUcbNode)
+    : backPropagate(tree)(rollout(strategy)(bestUcbNode.state), bestUcbNode)
+
+  console.log("NEWTREE=====>\n", newTree)
+  return newTree
+}
+
+export const findBestNodeIteration = (strategy: Strategy) => (tree2: Tree) => visit(strategy)(tree2, tree2.nodes[0])
+
+export const findBestNode = (strategy: Strategy) => (tree2: Tree) => {
+  const findBest = findBestNodeIteration(strategy)
+
+  const tree = R.range(1, 4).reduce(acc => findBest(acc), tree2)
+
+  const children = getChildren(tree)(getRoot(tree))
+  const maxValue = R.reduce(
+    R.max,
+    -Infinity,
+    children.map(c => c.value),
+  )
+  return { tree, node: children.find(c => c.value === maxValue)! }
 }
--- test/mcts.spec.ts
+++ test/mcts.spec.ts
@@ -2,125 +2,243 @@ import * as R from "ramda"
 import * as MCTS from "../src/mcts"
 import { DeepPartial } from "../src/utils/types"
 
-const defaultStrategy = {
+type ProblemMove = {
+  id: string
+  nextState: string
+}
+
+type ProblemMoves = { [k: string]: ProblemMove }
+type ProblemNode = {
+  final?: boolean | undefined
+  id: string
+  moves: ProblemMoves
+  rolloutMove?: string
+  value: number
+}
+
+type ProblemTree = { [k: string]: ProblemNode }
+
+const defaultStrategy: MCTS.Strategy = {
   availableMoves: () => [],
   calcUcb: () => 0,
   calcValue: () => 0,
-  getNextMove: () => ({}),
   isFinal: () => true,
-  nextState: (state: any) => state,
+  nextMove: () => undefined,
+  nextState: state => state,
 }
 
 const getStrategy = (overrides: DeepPartial<MCTS.Strategy> = {}) => R.mergeDeepRight(defaultStrategy, overrides)
 
+const buildStrategy = (problemTree: ProblemTree): MCTS.Strategy =>
+  getStrategy({
+    availableMoves: state => R.values(problemTree[state.id].moves).map(m => ({ id: m.id })),
+    calcUcb: node => (node.visits === 0 ? Infinity : node.value),
+    calcValue: state => problemTree[state.id].value,
+    isFinal: state => !!problemTree[state.id].final,
+    nextMove: state => {
+      const r = problemTree[state.id].rolloutMove
+      return r ? { id: r } : undefined
+    },
+    nextState: (state, move) => ({ id: problemTree[state.id].moves[move.id].nextState }),
+  })
+
 describe("mcts", () => {
-  it("creates a tree with an initial state", () => {
-    const initialState = { some: "state" } as any
-    const move1 = { some: "move1" } as any
-    const move2 = { some: "move2" } as any
-    const state1 = { some: "state1" } as any
-    const state2 = { some: "state2" } as any
-
-    const strategy = getStrategy({
-      availableMoves: () => [move1, move2],
-      nextState: jest
-        .fn()
-        .mockImplementationOnce(() => state1)
-        .mockImplementationOnce(() => state2),
-    })
-
-    const tree = MCTS.createTree(strategy)(initialState)
-
-    const expectedNode = {
-      children: [
-        {
-          children: [],
-          index: 1,
-          move: move1,
-          parentIndex: 0,
-          state: state1,
-          value: 0,
-          visits: 0,
+  describe("on first level", () => {
+    const problemTree: ProblemTree = {
+      s1: {
+        id: "s1",
+        moves: {
+          m2: {
+            id: "m2",
+            nextState: "s2",
+          },
+          m3: {
+            id: "m3",
+            nextState: "s3",
+          },
         },
-        {
-          children: [],
-          index: 2,
-          move: move2,
-          parentIndex: 0,
-          state: state2,
-          value: 0,
-          visits: 0,
-        },
-      ],
-      index: 0,
-      move: undefined,
-      parentIndex: MCTS.NO_PARENT,
-      state: initialState,
-      value: 0,
-      visits: 0,
-    }
-
-    const expectedTree = {
-      nodes: [expectedNode],
-    }
-
-    expect(tree).toEqual(expectedTree)
-  })
-
-  it("return the best move available if they are all final", () => {
-    const bestMove = { some: "bestMove" } as any
-    const otherMove = { some: "otherMove" } as any
-    const initialState = { some: "state" } as any
-
-    const strategy = getStrategy({
-      availableMoves: () => [otherMove, bestMove],
-      calcUcb: jest.fn(node => (node.move === bestMove ? 20 : 10)),
-      isFinal: () => true,
-    })
-
-    const tree = MCTS.createTree(strategy)(initialState)
-
-    const bestMoveFound = MCTS.findBestMove(strategy)(tree.nodes[0])
-
-    expect(bestMoveFound).toBe(bestMove)
-  })
-
-  it("rollsOut the best move available", () => {
-    type Indexed = { [k: string]: any }
-
-    const getStateInfo = (info: any) => stateInfo[info.id]!
-
-    const createStateInfo = (id: string, ucb: number, nextMove: string = ""): Indexed => ({
-      [id]: {
-        move: { id },
-        nextMove,
-        nextState: { id },
-        ucb,
+        value: 0,
       },
-    })
-
-    const initialState = { some: "state" } as any
-    const stateInfo = {
-      ...createStateInfo("best", 20, "rolloutFromBest"),
-      ...createStateInfo("other", 10, "rolloutFromOther"),
-      ...createStateInfo("rolloutFromBest", 15),
-      ...createStateInfo("rolloutFromOther", 3),
+      s2: {
+        final: true,
+        id: "s2",
+        moves: {},
+        value: 10,
+      },
+      s3: {
+        final: true,
+        id: "s3",
+        moves: {},
+        value: 15,
+      },
     }
 
-    const strategy = getStrategy({
-      availableMoves: () => [stateInfo.best.move, stateInfo.other.move],
-      calcUcb: node => getStateInfo(node.move).ucb,
-      getNextMove: jest.fn(state => stateInfo[getStateInfo(state).nextMove].move),
-      isFinal: state => state === stateInfo.rolloutFromBest.nextState,
-      nextState: (_, move) => getStateInfo(move).nextState,
+    it("creates a tree from an initial state", () => {
+      const strategy = buildStrategy(problemTree)
+
+      const tree = MCTS.createTree(strategy)({ id: problemTree.s1.id })
+
+      const expectedTree = {
+        nodes: [
+          {
+            children: [1, 2],
+            index: 0,
+            move: undefined,
+            parentIndex: MCTS.NO_PARENT,
+            state: { id: "s1" },
+            value: 0,
+            visits: 0,
+          },
+          {
+            children: [],
+            index: 1,
+            move: { id: "m2" },
+            parentIndex: 0,
+            state: { id: "s2" },
+            value: 0,
+            visits: 0,
+          },
+          {
+            children: [],
+            index: 2,
+            move: { id: "m3" },
+            parentIndex: 0,
+            state: { id: "s3" },
+            value: 0,
+            visits: 0,
+          },
+        ],
+      }
+
+      expect(tree).toEqual(expectedTree)
     })
 
-    const tree = MCTS.createTree(strategy)(initialState)
+    it("finds the best node with no rollouts", () => {
+      const strategy = buildStrategy(problemTree)
 
-    const bestMoveFound = MCTS.findBestMove(strategy)(tree.nodes[0])
+      const tree = MCTS.createTree(strategy)({ id: problemTree.s1.id })
 
-    expect(strategy.getNextMove).toHaveBeenCalledWith(stateInfo.best.nextState)
+      const { tree: newTree, node: bestNode } = MCTS.findBestNode(strategy)(tree)
 
-    expect(bestMoveFound).toBe(stateInfo.best.move)
+      expect(bestNode.move?.id).toBe("m3")
+      expect(bestNode.visits).toBe(1)
+      expect(bestNode.value).toBe(15)
+
+      const root = MCTS.getRoot(newTree)
+      expect(root.value).toBe(10 + 15)
+    })
+  })
+
+  it("find the best node after a rollout", () => {
+    const problemTree: ProblemTree = {
+      s0: {
+        id: "s0",
+        moves: {
+          m1: {
+            id: "m1",
+            nextState: "s1",
+          },
+          m2: {
+            id: "m2",
+            nextState: "s2",
+          },
+        },
+        value: 0,
+      },
+      s1: {
+        id: "s1",
+        moves: {
+          m12: {
+            id: "m12",
+            nextState: "s12",
+          },
+          m5: {
+            id: "m5",
+            nextState: "s5",
+          },
+          mr4: {
+            id: "mr4",
+            nextState: "sr4",
+          },
+        },
+        rolloutMove: "mr4",
+        value: 0,
+      },
+      s3: {
+        final: true,
+        id: "s3",
+        moves: {
+          m4: {
+            id: "m4",
+            nextState: "s4",
+          },
+          m5: {
+            id: "m5",
+            nextState: "s5",
+          },
+          mr5: {
+            id: "mr5",
+            nextState: "sr5",
+          },
+        },
+        rolloutMove: "mr5",
+        value: 7,
+      },
+      s4: {
+        id: "s4",
+        moves: {
+          m6: {
+            id: "m6",
+            nextState: "s6",
+          },
+        },
+        value: 0,
+      },
+      s5: {
+        id: "s2",
+        moves: {
+          m7: {
+            id: "m7",
+            nextState: "s7",
+          },
+        },
+        value: 50,
+      },
+      s6: {
+        final: true,
+        id: "s6",
+        moves: {},
+        value: 20,
+      },
+      s7: {
+        final: true,
+        id: "s7",
+        moves: {},
+        value: 30,
+      },
+      sr4: {
+        id: "sr4",
+        moves: {},
+        value: 55,
+      },
+      sr5: {
+        id: "sr5",
+        moves: {},
+        value: 35,
+      },
+    }
+
+    const strategy = buildStrategy(problemTree)
+
+    const tree = MCTS.createTree(strategy)({ id: problemTree.s1.id })
+
+    const { tree: newTree, node: bestNode } = MCTS.findBestNode(strategy)(tree)
+
+    expect(bestNode.move?.id).toBe("m3")
+    expect(bestNode.visits).toBe(1)
+    expect(bestNode.value).toBe(15)
+
+    const root = MCTS.getRoot(newTree)
+    expect(root.value).toBe(10 + 15)
   })
 })
