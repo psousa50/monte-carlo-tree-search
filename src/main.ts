e7c6894d2b54378a377790ee14c524363a808b0a
Pedro Sousa <pedronsousa@gmail.com>
2020-01-23 22:52:28 +0000

    Add TicTacToe

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,39 @@
-// import * as ThreeHoleGame from "../src/games/ThreeHoleGame"
-// import * as MCTS from "../src/mcts"
+import * as TicTacToe from "../src/games/TicTacToe"
+import * as MCTS from "../src/mcts"
 
-// const calcNodeValue = (state: ThreeHoleGame.GameState) =>
-//   state.holes[1] === "X" ? 1 : state.holes[1] === "O" ? -1 : undefined
+const calcNodeValue = (state: TicTacToe.GameState) =>
+  TicTacToe.playerWins(state.board, "X") ? 1 : TicTacToe.playerWins(state.board, "O") ? -1 : undefined
 
-// const strategy: MCTS.Strategy<ThreeHoleGame.GameState, ThreeHoleGame.Move> = {
-//   availableMoves: ThreeHoleGame.availableMoves,
-//   calcValue: calcNodeValue,
-//   isFinal: ThreeHoleGame.isFinal,
-//   nextMove: ThreeHoleGame.nextMove,
-//   nextState: ThreeHoleGame.move,
-// }
+const strategy: MCTS.Strategy<TicTacToe.GameState, TicTacToe.Move> = {
+  availableMoves: TicTacToe.availableMoves,
+  calcValue: calcNodeValue,
+  isFinal: TicTacToe.isFinal,
+  nextMove: TicTacToe.nextMove,
+  nextState: TicTacToe.move,
+}
 
 // const calcUcb = (_: MCTS.Tree) => (node: MCTS.Node) => 100 - node.visits
 
-// const config: MCTS.Config<ThreeHoleGame.GameState, ThreeHoleGame.Move> = {
-//   calcUcb,
-//   strategy,
-// }
+const config: MCTS.Config<TicTacToe.GameState, TicTacToe.Move> = {
+  calcUcb: MCTS.defaultUcbFormula(),
+  strategy,
+}
 
-// const run = () => {
+const run = () => {
 
-//   const game = ThreeHoleGame.create()
+  const game = TicTacToe.create()
 
-//   const { tree } = MCTS.findBestNode(config)(MCTS.createTree(game), 10)
+  const { tree, node } = MCTS.findBestNode(MCTS.createTree(config)(game), 60)
 
-//   const rootNodes = MCTS.getChildren(tree)(MCTS.getRoot(tree))
+  const rootNodes = MCTS.getChildren(tree)(MCTS.getRoot(tree))
 
-//   // console.log("FINAL TREE=====>\n", JSON.stringify(tree, null, 2))
-//   console.log("ROOT NODES=====>\n", JSON.stringify(rootNodes, null, 2))
+  // console.log("FINAL TREE=====>\n", JSON.stringify(tree, null, 2))
+  // console.log("ROOT NODES=====>\n", JSON.stringify(rootNodes, null, 2))
 
-// }
+  console.log("BEST NODE=====>\n", node)
 
-// run()
+}
 
-// // console.log("=====>\n", Math.log(2))
+run()
+
+// console.log("=====>\n", Math.log(2))
